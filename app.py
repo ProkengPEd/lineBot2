c30e9c185b84c773a9a4e04abbf2c70537f2a9a5
ProkengPEd <52600350+ProkengPEd@users.noreply.github.com>
2019-09-03 14:07:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
     if (userText == 'สวัสดี') :
         sendText(user,'จ้าา♥')
-    elif (userText == 'บ้ายบาย') :
+    elif (userText == 'บาย') :
         sendText(user,'บ้ายบายจ้าา')
     else :
         sendText(user,'มีไร ไม่เข้าใจ')
